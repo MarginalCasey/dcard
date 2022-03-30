62ef1b272d4bf18d6720398b09fcb92dfeb1c2fe
張文源 <c29429768@gmail.com>
2022-03-30 22:50:23 +0800

    Update: wrap Link with forwardRef

--- src/Link.js
+++ src/Link.js
@@ -1,27 +1,24 @@
-import React, { useRef, useEffect } from 'react'
+import React, { forwardRef, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-function Link({ id, url, cachedHeight, children, ...props }) {
-  const ref = useRef()
+const Link = forwardRef(
+  ({ id, url, cachedHeight, children, ...props }, ref) => {
+    const innerRef = useRef()
+    const linkRef = ref || innerRef
 
-  useEffect(() => {
-    if (cachedHeight[id] === undefined) {
-      cachedHeight[id] = ref.current.clientHeight
-    }
-  }, [])
+    useEffect(() => {
+      if (cachedHeight[id] === undefined) {
+        cachedHeight[id] = linkRef.current.clientHeight
+      }
+    }, [])
 
-  return (
-    <a
-      ref={ref}
-      href={url}
-      target="_blank"
-      rel="noreferrer"
-      {...props}
-    >
-      {children}
-    </a>
-  )
-}
+    return (
+      <a ref={linkRef} href={url} target="_blank" rel="noreferrer" {...props}>
+        {children}
+      </a>
+    )
+  }
+)
 
 Link.propTypes = {
   id: PropTypes.number,
